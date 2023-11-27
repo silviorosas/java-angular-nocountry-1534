2fc623cabbe3a4188140fea3d501f30cd41a9ce4
Silvio Rosas <silviorosas@yahoo.com.ar>
2023-11-27 10:49:19 -0300

    subo estructura back

---
+++ back/src/main/java/com/g1534/javaangular/JavaAngularApplication.java
@@ -0,0 +1,13 @@
+package com.g1534.javaangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAngularApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/g1534/javaangular/JavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g1534.javaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

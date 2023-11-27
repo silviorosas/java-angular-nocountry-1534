26f2680e2963bbe474165187580bd95f8b9e43f8
Silvio Rosas <silviorosas@yahoo.com.ar>
2023-11-27 13:05:21 -0300

    Agrego carpetas al backend

---
+++ back/src/main/java/com/g1534/javaangular/controller/ConsultaController.java
@@ -0,0 +1,16 @@
+package com.g1534.javaangular.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consulta")
+public class ConsultaController {
+
+    @GetMapping
+    public String hola(){
+        return "Hola No Country";
+    }
+}
